72209e0ffbafaa5fc8139257413595c0c1ef7168
flign <filipignas@gmail.com>
2022-08-27 12:56:29 +0300

    Tests using Swagger and Postman

--- StudentAdminPortal.API/StudentAdminPortal.API/Controllers/StudentsController.cs
+++ StudentAdminPortal.API/StudentAdminPortal.API/Controllers/StudentsController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StudentAdminPortal.API.DomainModels;
 using StudentAdminPortal.API.Repositories;
+using System.Collections.Generic;
 
 namespace StudentAdminPortal.API.Controllers
 {
@@ -17,7 +19,24 @@ namespace StudentAdminPortal.API.Controllers
         [Route("[controller]")]
         public IActionResult GetAllStudents()
         {
-            return Ok(studentRepository.GetStudents());
+            var students = studentRepository.GetStudents();
+            var domainModelStudents = new List<Student>();
+
+            foreach (var student in students)
+            {
+                domainModelStudents.Add(new Student()
+                {
+                    Id = student.Id,
+                    FirstName = student.FirstName,
+                    LastName = student.LastName,
+                    DateOfBirth = student.DateOfBirth,
+                    Email = student.Email,
+                    Mobile = student.Mobile,
+                    ProfileImageUrl = student.ProfileImageUrl,
+                    GenderId = student.GenderId,
+                });
+            }
+            return Ok(domainModelStudents);
         }
     }
 }
